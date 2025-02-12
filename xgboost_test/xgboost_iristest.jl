cb774b47059b5ad3047679d83d054e5b032ece79
PasoStudio73 <paso.studio73@gmail.com>
2025-02-12 15:21:48 +0100

    xgboost

--- xgboost_test/xgboost_iristest.jl
+++ xgboost_test/xgboost_iristest.jl
@@ -2,203 +2,205 @@ using CSV, DataFrames
 using MLJ, MLJBase
 using CategoricalArrays
 using XGBoost, MLJXGBoostInterface
-using Sole, SoleXplorer
+using SoleXplorer, SoleModels
 import MLJModelInterface as MMI
 using MLJDecisionTreeInterface
+using AbstractTrees
 
 x, y = @load_iris
 X = DataFrame(x)
-
 fixed_indices = [83, 12, 145, 22, 98, 125, 44, 3, 150, 67, 89, 31, 122, 15, 78, 99, 45, 11, 134, 52, 23, 120, 48, 88, 28, 4, 65, 17, 127, 49]
 train_indices = setdiff(1:150, fixed_indices)
-
 feature_names = names(X)
 labels       = unique(y)
 nlabels      = length(labels)
-
 X_train = X[train_indices, :]
 X_test  = X[fixed_indices, :]
 y_train = y[train_indices]
 y_test  = y[fixed_indices]
-
 y_coded_train = @. CategoricalArrays.levelcode(y_train) - 1 # convert to 0-based indexing
 y_coded_test  = @. CategoricalArrays.levelcode(y_test)  - 1 # convert to 0-based indexing
-
 dstrain = XGBoost.DMatrix((X_train, y_coded_train); feature_names)
 dstest  = XGBoost.DMatrix((X_test, y_coded_test);   feature_names)
 
-bst = XGBoost.xgboost(dstrain, num_round=10, max_depth=3, num_class=nlabels, objective="multi:softmax")
-
+# xgbmodel = XGBoost.xgboost(dstrain, num_round=1, max_depth=3, objective="multi:softprob", num_class=nlabels)
 # return AbstractTrees.jl compatible tree objects describing the model
-bst_tree = XGBoost.trees(bst)
+# xgbtree = XGBoost.trees(xgbmodel)
+# AbstractTrees.print_tree(xgbtree)
 
-# create and train a gradient boosted tree model of 5 trees
-bst = XGBoost.xgboost(dtrain, num_round=5, max_depth=6, objective="reg:squarederror")
-y_predict = XGBoost.predict(bst, dtest)
-
-# early stopping
-bst = XGBoost.xgboost(dtrain, 
-    num_round = 100, 
-    eval_metric = "rmse", 
-    watchlist = OrderedDict(["train" => dtrain, "eval" => dtest]), 
-    early_stopping_rounds = 5, 
-    max_depth=6, 
-    η=0.3
-)
-# get the best iteration and use it for prediction
-y_predict = XGBoost.predict(bst, dtest, ntree_limit = bst.best_iteration)
-
-bst = XGBoost.xgboost(dtrain, num_round=1; XGBoost.randomforest()...)
-
-# we can also retain / use the best score (based on eval_metric) which is stored in the booster
-println("Best RMSE from model training $(round((bst.best_score), digits = 8)).")
-
-prediction_rounded = round.(Int, y_predict)
-
-MLBase.errorrate(y_code[tt_pairs.test], prediction_rounded)
-MLBase.confusmat(length(levels(y_code)), Array(y_code[tt_pairs.test] .+ 1), Array(prediction_rounded) .+ 1)
-
-XGBoost.importancetable(bst)
-
-# return AbstractTrees.jl compatible tree objects describing the model
-bst_tree = XGBoost.trees(bst)
-
-AbstractTrees.repr_tree(bst_tree)
-AbstractTrees.print_tree(bst_tree)
-
-############à#######
-# function xgboost #
-####################
-params = (;
-    test                        = 1, 
-    num_round                   = 10, 
-    booster                     = "gbtree", 
-    disable_default_eval_metric = 0, 
-    eta                         = 0.3, 
-    num_parallel_tree           = 1, 
-    gamma                       = 0.0, 
-    max_depth                   = 3, 
-    min_child_weight            = 1.0, 
-    max_delta_step              = 0.0, 
-    subsample                   = 1.0, 
-    colsample_bytree            = 1.0, 
-    colsample_bylevel           = 1.0, 
-    colsample_bynode            = 1.0, 
-    lambda                      = 1.0, 
-    alpha                       = 0.0, 
-    tree_method                 = "auto", 
-    sketch_eps                  = 0.03, 
-    scale_pos_weight            = 1.0, 
-    updater                     = nothing, 
-    refresh_leaf                = 1, 
-    process_type                = "default", 
-    grow_policy                 = "depthwise", 
-    max_leaves                  = 0, 
-    max_bin                     = 256, 
-    predictor                   = "cpu_predictor", 
-    sample_type                 = "uniform", 
-    normalize_type              = "tree", 
-    rate_drop                   = 0.0, 
-    one_drop                    = 0, 
-    skip_drop                   = 0.0, 
-    feature_selector            = "cyclic", 
-    top_k                       = 0, 
-    tweedie_variance_power      = 1.5, 
-    objective                   = "automatic", 
-    base_score                  = 0.5, 
-    early_stopping_rounds       = 0, 
-    watchlist                   = nothing, 
-    nthread                     = 1, 
-    importance_type             = "gain", 
-    seed                        = nothing, 
-    validate_parameters         = false, 
-    eval_metric                 = String[]
-)
-
-classifier = MLJXGBoostInterface.XGBoostClassifier(; params...)
+# MLJXGBoostInterface
+classifier = MLJXGBoostInterface.XGBoostClassifier(num_round=1, max_depth=3, objective="multi:softprob")
 mach = MLJ.machine(classifier, X_train, y_train)
 fit!(mach, verbosity=0)
+#
 
-# learn_method = (
-#     (mach, X, y) -> begin
-        fitresult = SoleModels.fitted_params(mach)
+function get_condition(featidstr, featval, featurenames; test_operator)
+    featid = parse(Int, featidstr[2:end]) + 1 # considering 0-based indexing in XGBoost feature ids
+    feature = isnothing(featurenames) ? VariableValue(featid) : VariableValue(featid, featurenames[featid])
+    return ScalarCondition(feature, test_operator, featval)
+end
 
-        solemodel(fitresult.trees, fitresult.encoding)
+function satisfies_conditions(row::DataFrameRow, formula)
+    # check_cond = true
+    # for atom in formula
+    #     if !atom.value.metacond.test_operator(row[atom.value.metacond.feature.i_variable], atom.value.threshold)
+    #         check_cond = false
+    #     end
+    # end
+    # return check_cond
 
+    all(atom -> atom.value.metacond.test_operator(
+                    row[atom.value.metacond.feature.i_variable],
+                    atom.value.threshold), formula
+                )
+end
 
-plain_classifier = XGBoostClassifier(num_round=10, max_depth=3)
-m = machine(plain_classifier, X_train, y_train)
-fit!(m,verbosity = 0)
+function bitmap_check_conditions(X::DataFrame, formula)
+    BitVector([satisfies_conditions(row, formula) for row in eachrow(X)])
+end
 
-fitresult, cache, report = MLJBase.fit(plain_classifier, 0, X_train, y_train;)
-
-yhat = mode.(MLJ.predict(plain_classifier, fitresult, x_test))
-classification_rate = 1- sum(yhat .!= y_test)/length(y_test)
-
-
-
-classifier = MLJXGBoostInterface.XGBoostClassifier(; params...)
-mach = MLJ.machine(classifier, x_train, y_train)
-fit!(mach, verbosity=0)
-
-# learn_method = (
-#     (mach, X, y) -> begin
-        fitresult = SoleModels.fitted_params(mach)
-        dt = SoleModels.solemodel(fitresult.stumps, fitresult.encoding)
-
-# testset = modelset.learn_method(mach, Xtest[i], ytest[i])
-weights = mach.fitresult[2]
-classlabels = sort(mach.fitresult[3])
-featurenames = MLJ.report(mach).features
-dt = solemodel(MLJ.fitted_params(mach).stumps; weights, classlabels, featurenames)
-apply!(dt, X, y)
-
-
-#########################################
-
-# function DecisionTreeModel
-type = MLJDecisionTreeInterface.DecisionTreeClassifier
-config  = (; algo=:classification, type=DecisionTree, treatment=:aggregate)
-
-params = (;
-    max_depth              = -1,
-    min_samples_leaf       = 1, 
-    min_samples_split      = 2, 
-    min_purity_increase    = 0.0, 
-    n_subfeatures          = 0,
-    post_prune             = false,
-    merge_purity_threshold = 1.0,
-    display_depth          = 5,
-    feature_importance     = :impurity,
-    rng                    = Random.TaskLocalRNG()
+function pasomodel(
+    model::Vector{<:XGBoost.Node},
+    args...;
+    weights::Union{AbstractVector{<:Number}, Nothing}=nothing,
+    classlabels = nothing,
+    featurenames = nothing,
+    keep_condensed = false,
+    kwargs...
 )
+    # TODO
+    if keep_condensed && !isnothing(classlabels)
+        # info = (;
+        #     apply_preprocess=(y -> orig_O(findfirst(x -> x == y, classlabels))),
+        #     apply_postprocess=(y -> classlabels[y]),
+        # )
+        info = (;
+            apply_preprocess=(y -> findfirst(x -> x == y, classlabels)),
+            apply_postprocess=(y -> classlabels[y]),
+        )
+        keep_condensed = !keep_condensed
+        # O = eltype(classlabels)
+    else
+        info = (;)
+        # O = orig_O
+    end
+    
+    # trees = map(t -> pasomodel(t, args...; classlabels, featurenames, keep_condensed, kwargs...), model.trees)
+    trees = map(t -> pasomodel(t, args...; classlabels, featurenames, keep_condensed, kwargs...), model)
 
-winparams = (type=SoleBase.wholewindow,)
+    if !isnothing(featurenames)
+        info = merge(info, (; featurenames=featurenames, ))
+    end
 
-# learn_method = (
-#     (mach, X, y) -> (dt = solemodel(MLJ.fitted_params(mach).tree); apply!(dt, X, y); dt),
-#     (mach, X, y) -> (dt = solemodel(MLJ.fitted_params(mach).best_fitted_params.tree); apply!(dt, X, y); dt)
-# )
+    info = merge(info, (;
+            leaf_values=vcat([t.info[:leaf_values] for t in trees]...),
+            supporting_predictions=vcat([t.info[:supporting_predictions] for t in trees]...),
+            supporting_labels=vcat([t.info[:supporting_labels] for t in trees]...),
+        )
+    )
 
-# tuning = (
-#     tuning        = false,
-#     method        = (type = latinhypercube, ntour = 20),
-#     params        = TUNING_PARAMS,
-#     ranges        = [
-#         model -> MLJ.range(model, :merge_purity_threshold, lower=0, upper=1),
-#         model -> MLJ.range(model, :feature_importance, values=[:impurity, :split])
-#     ]
-# )
+    if isnothing(weights)
+        m = DecisionEnsemble(trees, info)
+    else
+        m = DecisionEnsemble(trees, weights, info)
+    end
+    return m
+end
 
-# rules_method = SoleModels.PlainRuleExtractor()
+"""
+    solemodel(tree::XGBoost.Node; fl=Formula[], fr=Formula[], classlabels=nothing, featurenames=nothing, keep_condensed=false)
 
-# classifier = getmodel(m)
-classifier = MLJDecisionTreeInterface.DecisionTreeClassifier(; params...)
+Traverses a learned XGBoost tree, collecting the path conditions for each branch. 
+Left paths (<) store conditions in `fl`, right paths (≥) store conditions in `fr`. 
+When reaching a leaf, calls `xgbleaf` with the path's collected conditions.
+"""
+function pasomodel(
+    tree::XGBoost.Node,
+    X::AbstractDataFrame,
+    y::AbstractVector;
+    path_conditions = Formula[],
+    classlabels=nothing,
+    featurenames=nothing,
+    keep_condensed=false
+)
+    keep_condensed && error("Cannot keep condensed XGBoost.Node.")
 
-# mach = fitmodel(m, classifier, ds);
-# model = testmodel(m, mach, ds);
-mach = MLJ.machine(classifier, x_train, y_train)
-fit!(mach, verbosity=0)
+    antecedent = Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(<)))
+    
+    # Create a new path for the left branch
+    left_path = copy(path_conditions)
+    push!(left_path, Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(<))))
+    
+    # Create a new path for the right branch
+    right_path = copy(path_conditions)
+    push!(right_path, Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(≥))))
+    
+    lefttree = if isnothing(tree.children[1].split)
+        # @show SoleModels.join_antecedents(left_path)
+        xgbleaf(tree.children[1], left_path, X, y; classlabels, featurenames)
+    else
+        pasomodel(tree.children[1], X, y; path_conditions=left_path, classlabels=classlabels, featurenames=featurenames)
+    end
+    
+    righttree = if isnothing(tree.children[2].split)
+        # @show SoleModels.join_antecedents(right_path)
+        xgbleaf(tree.children[2], right_path, X, y; classlabels, featurenames)
+    else
+        pasomodel(tree.children[2], X, y; path_conditions=right_path, classlabels=classlabels, featurenames=featurenames)
+    end
 
-fitresult = MMI.fitted_params(mach)
+    info = (;
+        leaf_values = [lefttree.info[:leaf_values]..., righttree.info[:leaf_values]...],
+        supporting_predictions = [lefttree.info[:supporting_predictions]..., righttree.info[:supporting_predictions]...],
+        supporting_labels = [lefttree.info[:supporting_labels]..., righttree.info[:supporting_labels]...],
+    )
+    return Branch(antecedent, lefttree, righttree, info)
+end
 
+function xgbleaf(
+    leaf::XGBoost.Node,
+    formula::Vector{<:Formula},
+    X::AbstractDataFrame,
+    y::AbstractVector;
+    classlabels=nothing,
+    featurenames=nothing,
+    keep_condensed=false
+)
+    keep_condensed && error("Cannot keep condensed XGBoost.Node.")
+
+    bitX = bitmap_check_conditions(X, formula)
+    prediction = SoleModels.bestguess(y[bitX])
+    labels = unique(y)
+
+    # if !isnothing(classlabels)
+    #     prediction = classlabels[prediction]
+    #     labels = classlabels[labels]
+    # end
+
+    info = (;
+        leaf_values = leaf.leaf,
+        supporting_predictions = fill(prediction, length(labels)),
+        supporting_labels = labels,
+    )
+    return SoleModels.ConstantModel(prediction, info)
+end
+
+# https://stats.stackexchange.com/questions/395697/what-is-an-intuitive-interpretation-of-the-leaf-values-in-xgboost-base-learners
+
+
+##### learn_method
+get_encoding(classes_seen) = Dict(MMI.int(c) => c for c in MMI.classes(classes_seen))
+get_classlabels(encoding) = [string(encoding[i]) for i in sort(keys(encoding) |> collect)]
+
+trees = XGBoost.trees(mach.fitresult[1])
+encoding = get_encoding(mach.fitresult[2])
+classlabels = get_classlabels(encoding)
+featurenames = mach.report.vals[1][1]
+dt = pasomodel(trees, X_train, y_train; classlabels, featurenames)
+apply!(dt, X_test, y_test)
+
+##### performance measures
+preds = MLJ.predict(mach, X_test)
+yhat = MLJ.mode.(preds)
+kp = MLJ.kappa(yhat, y_test)
+acc = MLJ.accuracy(yhat, y_test)
--- xgboost_test/xgboost_test.jl
+++ xgboost_test/xgboost_test.jl
@@ -2,159 +2,211 @@ using CSV, DataFrames
 using MLJ, MLJBase
 using CategoricalArrays
 using XGBoost, MLJXGBoostInterface
-using Sole, SoleXplorer
+using SoleXplorer, SoleModels
 import MLJModelInterface as MMI
-using MLJDecisionTreeInterface, Random
+using MLJDecisionTreeInterface
+using AbstractTrees, StatsBase, Random
 
-train_path = joinpath(@__DIR__, "train.csv")
-test_path = joinpath(@__DIR__, "test.csv")
+X, y       = SoleData.load_arff_dataset("NATOPS")
+train_seed = 11
+rng        = Random.Xoshiro(train_seed)
+Random.seed!(train_seed)
 
-dtrain = CSV.read(train_path, DataFrame)
-dtest = CSV.read(test_path, DataFrame)
+# downsize dataset
+num_cols_to_sample = 10
+num_rows_to_sample = 10
+chosen_cols = StatsBase.sample(rng, 1:size(X, 2), num_cols_to_sample; replace=false)
+chosen_rows = StatsBase.sample(rng, 1:size(X, 1), num_rows_to_sample; replace=false)
 
-# X, y = @load_iris
-# tt = MLJ.partition(eachindex(y), 0.2; shuffle=true, rng=1)
+X = X[chosen_rows, chosen_cols]
+y = y[chosen_rows]
 
-x_train = dtrain[:, 1:end-1]
-x_test = dtest[:, 1:end-1]
+m = SoleXplorer.XGBoostClassifierModel()
+ds = prepare_dataset(X, y, m)
 
-y_train = CategoricalArray(map(x -> x == 1 ? "yes" : "no", dtrain[:, end]))
-y_test = CategoricalArray(map(x -> x == 1 ? "yes" : "no", dtest[:, end]))
-
-plain_classifier = XGBoostClassifier(num_round=100, max_depth=3, seed=123)
-m = machine(plain_classifier, x_train, y_train)
-fit!(m,verbosity = 0)
-
-fitresult, cache, report = MLJBase.fit(plain_classifier, 0, x_train, y_train;)
-
-yhat = mode.(MLJ.predict(plain_classifier, fitresult, x_test))
-classification_rate = 1- sum(yhat .!= y_test)/length(y_test)
-
-#########################################
-
-# function xgboost
-type   = MLJXGBoostInterface.XGBoostClassifier
-config = (; algo=:classification, type=SoleXplorer.DecisionEnsemble, treatment=:aggregate)
-
-params = (;
-    test                        = 1, 
-    num_round                   = 100, 
-    booster                     = "gbtree", 
-    disable_default_eval_metric = 0, 
-    eta                         = 0.3, 
-    num_parallel_tree           = 1, 
-    gamma                       = 0.0, 
-    max_depth                   = 3, 
-    min_child_weight            = 1.0, 
-    max_delta_step              = 0.0, 
-    subsample                   = 1.0, 
-    colsample_bytree            = 1.0, 
-    colsample_bylevel           = 1.0, 
-    colsample_bynode            = 1.0, 
-    lambda                      = 1.0, 
-    alpha                       = 0.0, 
-    tree_method                 = "auto", 
-    sketch_eps                  = 0.03, 
-    scale_pos_weight            = 1.0, 
-    updater                     = nothing, 
-    refresh_leaf                = 1, 
-    process_type                = "default", 
-    grow_policy                 = "depthwise", 
-    max_leaves                  = 0, 
-    max_bin                     = 256, 
-    predictor                   = "cpu_predictor", 
-    sample_type                 = "uniform", 
-    normalize_type              = "tree", 
-    rate_drop                   = 0.0, 
-    one_drop                    = 0, 
-    skip_drop                   = 0.0, 
-    feature_selector            = "cyclic", 
-    top_k                       = 0, 
-    tweedie_variance_power      = 1.5, 
-    # objective                   = "automatic", 
-    objective                   = "binary:logistic", 
-    base_score                  = 0.5, 
-    early_stopping_rounds       = 0, 
-    watchlist                   = nothing, 
-    nthread                     = 1, 
-    importance_type             = "gain", 
-    seed                        = nothing, 
-    validate_parameters         = false, 
-    eval_metric                 = String[]
-)
-
-# tuning = (
-#     tuning        = true,
-#     method        = (type = latinhypercube, ntour = 20),
-#     params        = SoleXplorer.TUNING_PARAMS,
-#     ranges        = [
-#         model -> MLJ.range(model, :max_depth, lower=3, upper=6),
-#         model -> MLJ.range(model, :sample_type, values=["uniform", "weighted"])
-#     ]
-# )
-
-classifier = MLJXGBoostInterface.XGBoostClassifier(; params...)
-mach = MLJ.machine(classifier, x_train, y_train)
+# MLJXGBoostInterface
+classifier = MLJXGBoostInterface.XGBoostClassifier(num_round=1, max_depth=3, objective="multi:softprob")
+mach = MLJ.machine(classifier, ds.Xtrain, ds.ytrain)
 fit!(mach, verbosity=0)
+#
 
-# learn_method = (
-#     (mach, X, y) -> begin
-        fitresult = SoleModels.fitted_params(mach)
-        dt = SoleModels.solemodel(fitresult.stumps, fitresult.encoding)
+function get_condition(featidstr, featval, featurenames; test_operator)
+    featid = parse(Int, featidstr[2:end]) + 1 # considering 0-based indexing in XGBoost feature ids
+    feature = isnothing(featurenames) ? VariableValue(featid) : VariableValue(featid, featurenames[featid])
+    return ScalarCondition(feature, test_operator, featval)
+end
 
-# testset = modelset.learn_method(mach, Xtest[i], ytest[i])
-weights = mach.fitresult[2]
-classlabels = sort(mach.fitresult[3])
-featurenames = MLJ.report(mach).features
-dt = solemodel(MLJ.fitted_params(mach).stumps; weights, classlabels, featurenames)
-apply!(dt, X, y)
+function satisfies_conditions(row::DataFrameRow, formula)
+    # check_cond = true
+    # for atom in formula
+    #     if !atom.value.metacond.test_operator(row[atom.value.metacond.feature.i_variable], atom.value.threshold)
+    #         check_cond = false
+    #     end
+    # end
+    # return check_cond
 
+    all(atom -> atom.value.metacond.test_operator(
+                    row[atom.value.metacond.feature.i_variable],
+                    atom.value.threshold), formula
+                )
+end
 
-#########################################
+function bitmap_check_conditions(X, formula)
+    BitVector([satisfies_conditions(row, formula) for row in eachrow(X)])
+end
 
-# function DecisionTreeModel
-type = MLJDecisionTreeInterface.DecisionTreeClassifier
-config  = (; algo=:classification, type=DecisionTree, treatment=:aggregate)
-
-params = (;
-    max_depth              = -1,
-    min_samples_leaf       = 1, 
-    min_samples_split      = 2, 
-    min_purity_increase    = 0.0, 
-    n_subfeatures          = 0,
-    post_prune             = false,
-    merge_purity_threshold = 1.0,
-    display_depth          = 5,
-    feature_importance     = :impurity,
-    rng                    = Random.TaskLocalRNG()
+function pasomodel(
+    model::Vector{<:XGBoost.Node},
+    args...;
+    weights::Union{AbstractVector{<:Number}, Nothing}=nothing,
+    classlabels = nothing,
+    featurenames = nothing,
+    keep_condensed = false,
+    kwargs...
 )
+    # TODO
+    if keep_condensed && !isnothing(classlabels)
+        # info = (;
+        #     apply_preprocess=(y -> orig_O(findfirst(x -> x == y, classlabels))),
+        #     apply_postprocess=(y -> classlabels[y]),
+        # )
+        info = (;
+            apply_preprocess=(y -> findfirst(x -> x == y, classlabels)),
+            apply_postprocess=(y -> classlabels[y]),
+        )
+        keep_condensed = !keep_condensed
+        # O = eltype(classlabels)
+    else
+        info = (;)
+        # O = orig_O
+    end
 
-winparams = (type=SoleBase.wholewindow,)
+    trees = filter(!isnothing, map(t -> pasomodel(t, args...; classlabels, featurenames, keep_condensed, kwargs...), model))
 
-# learn_method = (
-#     (mach, X, y) -> (dt = solemodel(MLJ.fitted_params(mach).tree); apply!(dt, X, y); dt),
-#     (mach, X, y) -> (dt = solemodel(MLJ.fitted_params(mach).best_fitted_params.tree); apply!(dt, X, y); dt)
-# )
 
-# tuning = (
-#     tuning        = false,
-#     method        = (type = latinhypercube, ntour = 20),
-#     params        = TUNING_PARAMS,
-#     ranges        = [
-#         model -> MLJ.range(model, :merge_purity_threshold, lower=0, upper=1),
-#         model -> MLJ.range(model, :feature_importance, values=[:impurity, :split])
-#     ]
-# )
+    if !isnothing(featurenames)
+        info = merge(info, (; featurenames=featurenames, ))
+    end
 
-# rules_method = SoleModels.PlainRuleExtractor()
+    info = merge(info, (;
+            leaf_values=vcat([t.info[:leaf_values] for t in trees]...),
+            supporting_predictions=vcat([t.info[:supporting_predictions] for t in trees]...),
+            supporting_labels=vcat([t.info[:supporting_labels] for t in trees]...),
+        )
+    )
 
-# classifier = getmodel(m)
-classifier = MLJDecisionTreeInterface.DecisionTreeClassifier(; params...)
+    if isnothing(weights)
+        m = DecisionEnsemble(trees, info)
+    else
+        m = DecisionEnsemble(trees, weights, info)
+    end
+    return m
+end
 
-# mach = fitmodel(m, classifier, ds);
-# model = testmodel(m, mach, ds);
-mach = MLJ.machine(classifier, x_train, y_train)
-fit!(mach, verbosity=0)
+"""
+    solemodel(tree::XGBoost.Node; fl=Formula[], fr=Formula[], classlabels=nothing, featurenames=nothing, keep_condensed=false)
 
-fitresult = MMI.fitted_params(mach)
+Traverses a learned XGBoost tree, collecting the path conditions for each branch. 
+Left paths (<) store conditions in `fl`, right paths (≥) store conditions in `fr`. 
+When reaching a leaf, calls `xgbleaf` with the path's collected conditions.
+"""
+function pasomodel(
+    tree::XGBoost.Node,
+    X::AbstractDataFrame,
+    y::AbstractVector;
+    path_conditions = Formula[],
+    classlabels=nothing,
+    featurenames=nothing,
+    keep_condensed=false
+)
+    keep_condensed && error("Cannot keep condensed XGBoost.Node.")
 
+    # xgboost trees could be composed of only one leaf, without any split
+    isnothing(tree.split) && return nothing
+
+    antecedent = Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(<)))
+    
+    # Create a new path for the left branch
+    left_path = copy(path_conditions)
+    push!(left_path, Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(<))))
+    
+    # Create a new path for the right branch
+    right_path = copy(path_conditions)
+    push!(right_path, Atom(get_condition(tree.split, tree.split_condition, featurenames; test_operator=(≥))))
+    
+    lefttree = if isnothing(tree.children[1].split)
+        # @show SoleModels.join_antecedents(left_path)
+        xgbleaf(tree.children[1], left_path, X, y; classlabels, featurenames)
+    else
+        pasomodel(tree.children[1], X, y; path_conditions=left_path, classlabels=classlabels, featurenames=featurenames)
+    end
+    
+    righttree = if isnothing(tree.children[2].split)
+        # @show SoleModels.join_antecedents(right_path)
+        xgbleaf(tree.children[2], right_path, X, y; classlabels, featurenames)
+    else
+        pasomodel(tree.children[2], X, y; path_conditions=right_path, classlabels=classlabels, featurenames=featurenames)
+    end
+
+    info = (;
+        leaf_values = [lefttree.info[:leaf_values]..., righttree.info[:leaf_values]...],
+        supporting_predictions = [lefttree.info[:supporting_predictions]..., righttree.info[:supporting_predictions]...],
+        supporting_labels = [lefttree.info[:supporting_labels]..., righttree.info[:supporting_labels]...],
+    )
+    return Branch(antecedent, lefttree, righttree, info)
+end
+
+function xgbleaf(
+    leaf::XGBoost.Node,
+    formula::Vector{<:Formula},
+    X::AbstractDataFrame,
+    y::AbstractVector;
+    classlabels=nothing,
+    featurenames=nothing,
+    keep_condensed=false
+)
+    keep_condensed && error("Cannot keep condensed XGBoost.Node.")
+
+    bitX = bitmap_check_conditions(X, formula)
+    prediction = SoleModels.bestguess(y[bitX])
+    labels = unique(y)
+
+    # if !isnothing(classlabels)
+    #     prediction = classlabels[prediction]
+    #     labels = classlabels[labels]
+    # end
+
+    info = (;
+        leaf_values = leaf.leaf,
+        supporting_predictions = fill(prediction, length(labels)),
+        supporting_labels = labels,
+    )
+    return SoleModels.ConstantModel(prediction, info)
+end
+
+##### learn_method
+get_encoding(classes_seen) = Dict(MMI.int(c) => c for c in MMI.classes(classes_seen))
+get_classlabels(encoding) = [string(encoding[i]) for i in sort(keys(encoding) |> collect)]
+
+trees = XGBoost.trees(mach.fitresult[1])
+encoding = get_encoding(mach.fitresult[2])
+classlabels = get_classlabels(encoding)
+featurenames = mach.report.vals[1][1]
+dt = pasomodel(trees, ds.Xtrain, ds.ytrain; classlabels, featurenames)
+apply!(dt, ds.Xtest, ds.ytest)
+
+##### performance measures
+preds = MLJ.predict(mach, ds.Xtest)
+yhat = MLJ.mode.(preds)
+kp = MLJ.kappa(yhat, ds.ytest)
+acc = MLJ.accuracy(yhat, ds.ytest)
+
+#####
+feature_names = names(ds.Xtest)
+y_coded_train = @. CategoricalArrays.levelcode(ds.ytrain) - 1 # convert to 0-based indexing
+y_coded_test  = @. CategoricalArrays.levelcode(ds.ytest)  - 1 # convert to 0-based indexing
+dstrain = XGBoost.DMatrix((ds.Xtrain, y_coded_train); feature_names)
+dstest  = XGBoost.DMatrix((ds.Xtest, y_coded_test); feature_names)
+nlabels = 6
+xgbmodel = XGBoost.xgboost(dstrain, num_round=1, max_depth=3, objective="multi:softprob", num_class=nlabels)
+xgbtree = XGBoost.trees(xgbmodel)
